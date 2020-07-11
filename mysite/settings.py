733627866f91c2f34cb8948c5f39746474a3df6d
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-07-11 13:03:46 +0600

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mainulislamfaruqi@gmail.com'
+EMAIL_HOST_PASSWORD = '01795115755MIF'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
