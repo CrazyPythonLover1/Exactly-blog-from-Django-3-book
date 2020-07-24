440a6ed3da2807037ebf3e2edb6c0318e5dee038
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-07-25 04:42:41 +0600

    add most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from ..models import Post 
+from django.db.models import Count
+
 
 register = template.Library()
    
@@ -19,3 +21,10 @@ simple_tag(name='my_tag').
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts':latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments = Count('comments')).order_by('-total_comments')[:count]
+
+
+
