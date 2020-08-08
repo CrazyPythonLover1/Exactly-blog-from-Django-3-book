855f09c71cc84846abe483d395fd2930eec68194
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-09 04:12:04 +0600

    design completed

--- blog/forms.py
+++ blog/forms.py
@@ -7,10 +7,23 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,widget=forms.Textarea)
 
+# class CommentForm(forms.Form):
+#     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+#     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+#     body = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
+    
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name','email', 'body')
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body':forms.Textarea(attrs={'class':'form-control'})
+        }
+
 class SearchForm(forms.Form):
-    query = forms.CharField()
+    query = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
